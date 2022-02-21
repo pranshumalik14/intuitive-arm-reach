ce18f77bdf6c529fd32675251805695f08a0351b
Pranshu Malik <pranshumalik14@gmail.com>
2022-02-21 15:18:47 -0500

    Divided realsense implementation into functions that find the origin; find calibrated origin over 5 seconds; and views the sensors in color, infrared, threshold, keypoints

--- src/vision/test_realsense.py
+++ src/vision/test_realsense.py
@@ -8,33 +8,71 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import time
 
-# set frame resolution
-resolutionWidth = 848
-resolutionHeight = 480
-frameRate = 30
 
-# Configure depth and color streams
-pipeline = rs.pipeline()
-config = rs.config()
+def keypoints_average(keypoints):
+    num, sum_x, sum_y = 0, 0, 0
 
-# Get device product line for setting a supporting resolution
-config.enable_stream(rs.stream.depth, resolutionWidth,
-                     resolutionHeight, rs.format.z16, frameRate)
-config.enable_stream(rs.stream.color, resolutionWidth,
-                     resolutionHeight, rs.format.bgr8, frameRate)
-config.enable_stream(rs.stream.infrared, 1, resolutionWidth,
-                     resolutionHeight, rs.format.y8, frameRate)
+    for keyp in keypoints:
+        num += 1
+        sum_x += keyp.pt[0]
+        sum_y += keyp.pt[1]
+    ave_x = sum_x/num
+    ave_y = sum_y/num
+    return round(ave_x), round(ave_y)
 
-# Start streaming
-pipeline.start(config)
 
-try:
+def blob_param():
+    # blob params
+    params = cv2.SimpleBlobDetector_Params()
+
+    # thresholds
+    params.filterByColor = False
+    params.minThreshold = 10
+    params.maxThreshold = 200
+    # params.thresholdStep = 20
+
+    # filter by area
+    params.filterByArea = True
+    params.minArea = 1
+    params.maxArea = 10000
+
+    # filter by circularity
+    params.filterByCircularity = False
+
+    # filter by convexity
+    params.filterByConvexity = False
+
+    # filter by inertia
+    params.filterByInertia = False
+    return params
+
+
+def set_config():
+    # set frame resolution
+    resolutionWidth = 848
+    resolutionHeight = 480
+    frameRate = 30
+    config = rs.config()
+
+    # Get device product line for setting a supporting resolution
+    config.enable_stream(rs.stream.depth, resolutionWidth,
+                         resolutionHeight, rs.format.z16, frameRate)
+    config.enable_stream(rs.stream.color, resolutionWidth,
+                         resolutionHeight, rs.format.bgr8, frameRate)
+    config.enable_stream(rs.stream.infrared, 1, resolutionWidth,
+                         resolutionHeight, rs.format.y8, frameRate)
+    return config
+
+
+def view_stream(pipeline, show_origin='True'):
+    config = set_config()
+    pipeline.start(config)
     while True:
 
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
-
         depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
         infra_frame = frames.get_infrared_frame()
@@ -51,54 +89,96 @@ try:
         infra_thresh[150:250, 530:625] = infra_image[150:250, 530:625]
 
         # blob params
-        params = cv2.SimpleBlobDetector_Params()
-
-        # Change thresholds
-        params.minThreshold = 60
-        params.maxThreshold = 255
-
-        # Filter by Area
-        params.filterByArea = True
-        params.minArea = 1500
-
-        # Filter by Circularity
-        params.filterByCircularity = True
-        params.minCircularity = 0.1
-
-        # Filter by Convexity
-        params.filterByConvexity = True
-        params.minConvexity = 0.87
-
-        # Filter by Inertia
-        params.filterByInertia = True
-        params.minInertiaRatio = 0.01
+        params = blob_param()
 
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(
             depth_image, alpha=0.03), cv2.COLORMAP_TURBO)
         infra_colormap = cv2.applyColorMap(infra_image, cv2.COLORMAP_HOT)
         _, infra_thresh = cv2.threshold(
-            infra_thresh, 50, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            infra_thresh, 50, 255, cv2.THRESH_BINARY)
 
-        depth_colormap_dim = depth_colormap.shape
-        infra_colormap_dim = infra_colormap.shape
-        color_colormap_dim = color_image.shape
+        detector = cv2.SimpleBlobDetector_create(params)
+        keypoints = detector.detect(infra_thresh)
+        im_with_keypoints = cv2.drawKeypoints(infra_thresh, keypoints, None, color=(
+            0, 0, 255), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
+        # Show keypoints
         # Show images
+        if (show_origin == 'True'):
+            ave_x, ave_y = keypoints_average(keypoints)
+            radius = 10
+            drawcolor = (0, 255, 0)  # Green
+            circlecenter = (ave_x, ave_y)
+            thickness = 2
+            image = cv2.circle(color_image, circlecenter,
+                               radius, drawcolor, thickness)
+            image = cv2.line(image, (ave_x-25, ave_y),
+                             (ave_x+25, ave_y), drawcolor, thickness)
+            image = cv2.line(image, (ave_x, ave_y-25),
+                             (ave_x, ave_y+25), drawcolor, thickness)
+
         cv2.namedWindow('Color', cv2.WINDOW_AUTOSIZE)
         cv2.namedWindow('Infra', cv2.WINDOW_AUTOSIZE)
         cv2.namedWindow('Depth', cv2.WINDOW_AUTOSIZE)
         cv2.namedWindow('Thresh', cv2.WINDOW_AUTOSIZE)
+        cv2.namedWindow('Keypoints', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow("Keypoints", im_with_keypoints)
         cv2.imshow('Color', color_image)
         cv2.imshow('Infra', infra_colormap)
         cv2.imshow('Depth', depth_colormap)
         cv2.imshow('Thresh', infra_thresh)
         cv2.waitKey(1)
+    print("View stream completed")
 
-except:
-    print("Error")
 
-finally:
+def return_origin(pipeline):
+    config = set_config()
+    pipeline.start(config)
 
-    # Stop streaming
+    # Wait for a coherent pair of frames: depth and color
+    frames = pipeline.wait_for_frames()
+    infra_frame = frames.get_infrared_frame()
+    while not infra_frame:
+        infra_frame = frames.get_infrared_frame()
+
+    # Convert images to numpy arrays
+    infra_image = np.asanyarray(infra_frame.get_data())
+
+    # crop out calibration area
+    infra_thresh = np.zeros_like(infra_image)
+    infra_thresh[150:250, 530:625] = infra_image[150:250, 530:625]
+
+    # blob params
+    params = blob_param()
+
+    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+    infra_colormap = cv2.applyColorMap(infra_image, cv2.COLORMAP_HOT)
+    _, infra_thresh = cv2.threshold(
+        infra_thresh, 50, 255, cv2.THRESH_BINARY)
+
+    detector = cv2.SimpleBlobDetector_create(params)
+    keypoints = detector.detect(infra_thresh)
+
+    # Show keypoints
+    # Show images
+    ave_x, ave_y = keypoints_average(keypoints)
     pipeline.stop()
+    return ave_x, ave_y
+
+
+def calibrated_origin(pipeline):
+    sum_x, sum_y = 0, 0
+    for i in range(0, 5):
+        ave_x, ave_y = return_origin(pipeline)
+        sum_x += ave_x
+        sum_y += ave_y
+        time.sleep(1)
+    ave_x = sum_x/5
+    ave_y = sum_y/5
+    return round(ave_x), round(ave_y)
+
+
+pipeline = rs.pipeline()
+print(calibrated_origin(pipeline))
+view_stream(pipeline)  # , show_origin = 'False')
