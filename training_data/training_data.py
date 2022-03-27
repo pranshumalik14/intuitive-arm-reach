4a76f9952dbee0c1a48a9b56e05a9987612c2d23
varunsampat30 <varunsampat30@gmail.com>
2022-03-27 00:11:59 -0400

    Varun Prithvi debug interpolator (pair programming)

--- training_data/training_data.py
+++ training_data/training_data.py
@@ -68,6 +68,40 @@ def generate_2D_target_pose(robot_arm, d_rho = 0.01, d_phi = (np.pi/10)):
     
     return np.array(Xs), np.array(Ys)
 
+
+def iterate_circular(buf, start_index, iter_by, cw=True):
+    buf_size = len(buf)
+    ret = []
+    for i in range(0, iter_by):
+        index = None
+        if cw:
+            index = (start_index - i)%buf_size
+        else:
+            index = (start_index + i)%buf_size
+        ret.append(buf[index])
+    return np.array(ret)
+
+def find_closest_target_pt(X_target, Y_target, x_ee, y_ee):
+    n_target_pts = len(X_target)
+
+    closest_idx = 0 
+    min_dist = np.Inf
+
+    print("End effector pose is: ({}, {})".format(x_ee, y_ee))
+
+    def euc_dist(x1, y1, x2, y2):
+        return np.sqrt( (y2-y1)**2 + (x2-x1)**2 )
+    for i in range(n_target_pts):
+        dist = euc_dist(X_target[i], Y_target[i], x_ee, y_ee)
+
+        if dist < min_dist:
+            min_dist = dist
+            closest_idx = i
+    
+    print("Closest point is ({}, {})".format(X_target[closest_idx], Y_target[closest_idx]))
+
+    return closest_idx
+
 def visualize_target_poses(Xs, Ys):
     # robot_arm = rb.RobotArm2D(
     #     n_dims = 3,
@@ -77,8 +111,7 @@ def visualize_target_poses(Xs, Ys):
     # Xs, Ys = generate_2D_target_pose(robot_arm)
 
     plt.scatter(Xs, Ys)
-    plt.savefig('training_data_sc_plot.png')
-
+    plt.savefig('training_data_sc_plot(1).png')
 
 def generate_random_init_joint_angle(robot_arm):
     """
@@ -115,8 +148,11 @@ def gen_training_data(robot_arm, n_joint_config = 10):
     X_target, Y_target = generate_2D_target_pose(
         robot_arm,
         d_rho = 0.1,
-        d_phi = np.pi/8
+        d_phi = np.pi/16
         )
+
+    visualize_target_poses(X_target, Y_target)
+
     assert(len(X_target) == len(Y_target))
     n_target_pts = len(X_target)
 
@@ -134,17 +170,32 @@ def gen_training_data(robot_arm, n_joint_config = 10):
         init_joint_angles = generate_random_init_joint_angle(robot_arm)
         # init_joint_angles_str = np.array2string(init_joint_angles[0], separator="|") # want only joint angles, not velocity since it is 0
 
-        Theta_matrix = np.zeros(B*N).reshape((B, N))
-        # Theta matrix is set to 0 everytime starting from new joint. TODO: ask Pranshu
-        for j in range(n_target_pts):
+        print("Initial joint angles are :")
+        print(init_joint_angles[0])
 
+        # call forward kinematics here, to determine the closest point
+        x_ee, y_ee = robot_arm.forward_kinematics(init_joint_angles[0])
+
+        closest_idx = find_closest_target_pt(X_target, Y_target, x_ee, y_ee)
+
+        # Get x,y cw
+        X_buf_cw = iterate_circular(X_target, closest_idx, n_target_pts//2, cw=True)
+        Y_buf_cw = iterate_circular(Y_target, closest_idx, n_target_pts//2, cw=True)
+
+        # Get x,y ccw
+        X_buf_ccw = iterate_circular(X_target, closest_idx, n_target_pts//2, cw=False)
+        Y_buf_ccw = iterate_circular(Y_target, closest_idx, n_target_pts//2, cw=False)
+
+
+        Theta_matrix = np.zeros(B*N).reshape((B, N))
+        for j in range(n_target_pts//2):
             index = (i * n_target_pts) + j
             final_training_data[index, 0] = init_joint_angles[0]
-            final_training_data[index, 1] = X_target[j]
-            final_training_data[index, 2] = Y_target[j]
+            final_training_data[index, 1] = X_buf_cw[j]
+            final_training_data[index, 2] = Y_buf_cw[j]
             
             Theta_matrix, iter_count, J_hist, task_info = pdff_sim.gen_theta(
-                x_target = np.array([X_target[j], Y_target[j]]),
+                x_target = np.array([X_buf_cw[j], Y_buf_cw[j]]),
                 init_condit = init_joint_angles,
                 robot_arm = robot_arm,
                 B = B,
@@ -155,6 +206,47 @@ def gen_training_data(robot_arm, n_joint_config = 10):
             final_training_data[index, 3] = Theta_matrix
             final_training_data[index, 4] = iter_count
             final_training_data[index, 5] = J_hist[-1]
+
+        Theta_matrix = np.zeros(B*N).reshape((B, N))
+        for j in range(n_target_pts//2):
+            index = n_target_pts//2 + (i * n_target_pts) + j + 1
+            final_training_data[index, 0] = init_joint_angles[0]
+            final_training_data[index, 1] = X_buf_ccw[j]
+            final_training_data[index, 2] = Y_buf_ccw[j]
+            
+            Theta_matrix, iter_count, J_hist, task_info = pdff_sim.gen_theta(
+                x_target = np.array([X_buf_ccw[j], Y_buf_ccw[j]]),
+                init_condit = init_joint_angles,
+                robot_arm = robot_arm,
+                B = B,
+                N = N,
+                Theta_matrix = Theta_matrix 
+            )
+
+            final_training_data[index, 3] = Theta_matrix
+            final_training_data[index, 4] = iter_count
+            final_training_data[index, 5] = J_hist[-1]
+        # Theta_matrix = np.zeros(B*N).reshape((B, N))
+        # # Theta matrix is set to 0 everytime starting from new joint. TODO: ask Pranshu
+        # for j in range(n_target_pts):
+
+        #     index = (i * n_target_pts) + j
+        #     final_training_data[index, 0] = init_joint_angles[0]
+        #     final_training_data[index, 1] = X_target[j]
+        #     final_training_data[index, 2] = Y_target[j]
+            
+        #     Theta_matrix, iter_count, J_hist, task_info = pdff_sim.gen_theta(
+        #         x_target = np.array([X_target[j], Y_target[j]]),
+        #         init_condit = init_joint_angles,
+        #         robot_arm = robot_arm,
+        #         B = B,
+        #         N = N,
+        #         Theta_matrix = Theta_matrix 
+        #     )
+
+        #     final_training_data[index, 3] = Theta_matrix
+        #     final_training_data[index, 4] = iter_count
+        #     final_training_data[index, 5] = J_hist[-1]
 
     task_info_df = task_info.to_pd()
     df = pd.DataFrame(
